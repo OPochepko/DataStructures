8e179726db93ce4f3840e47dd040082611c01d68
pochepko_oleg <pochepkooleg@gmail.com>
2019-11-18 20:27:34 +0300

    JUnit dependencies added, unit test for LinkedList

---
+++ src/main/java/ArrayList.java
@@ -0,0 +1,33 @@
+public class ArrayList implements List {
+
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove() {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+}
---
+++ src/main/java/List.java
@@ -0,0 +1,18 @@
+public interface List<E> {
+
+    int size();
+
+    boolean add(E e);
+
+    boolean remove();
+
+    boolean remove(Object o);
+
+    E get(int index);
+
+    void add(int index, E element);
+
+
+
+
+}
